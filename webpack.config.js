3e008d354f7127917c25b59d62e03c2197e7947c
Ryan Burnette <ryan.burnette@gmail.com>
2022-02-26 14:30:36 -0500

    v1

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.mjs',
+  entry: path.resolve(__dirname, './src/remove-google-oidc-query-params.mjs'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'remove-google-oidc-query-params.js',
